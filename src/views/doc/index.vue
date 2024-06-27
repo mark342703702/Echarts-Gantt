bc430722ee26eb78ca1ac32257a2abc59cbf8416
黄海 <huanghai@scjz.cc>
2024-06-27 14:38:45 +0800

    补充路由

--- src/views/doc/index.vue
+++ src/views/doc/index.vue
@@ -4,7 +4,12 @@
       <router-link :to="{ path: '/base' }">基础甘特图</router-link>
       <router-link :to="{ path: '/duration' }">带进度条甘特图</router-link>
       <router-link :to="{ path: '/drag' }">可拖拽甘特图</router-link>
+      <router-link :to="{ path: '/taskRender' }">自定义色块样式</router-link>
+      <router-link :to="{ path: '/menu' }">自定义右键菜单</router-link>
+      <router-link :to="{ path: '/tooltipRender' }">自定义tooltip</router-link>
+      <router-link :to="{ path: '/multichoose' }">多选操作</router-link>
     </div>
+    <div></div>
   </div>
 </template>
 
